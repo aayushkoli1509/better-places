a6d0351b8752343d30bb9b8fb39441b951628230
Mubin Ansari <mubinansari910@gmail.com>
2022-10-15 19:09:55 +0530

    connect signup controller to mongodb

--- backend/controllers/usersController.ts
+++ backend/controllers/usersController.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import HttpError from '../models/httpError.js';
 import { TypedRequest } from '../types';
-import { nanoid } from 'nanoid';
 import { validationResult } from 'express-validator';
+import User from '../models/user.js';
 
 const USERS = [
   {
@@ -17,11 +17,12 @@ const getUsers = (_: Request, res: Response) => {
   res.json({ users: USERS });
 };
 
-const signup = (
+const signup = async (
   req: TypedRequest<{
     name: string;
     email: string;
     password: string;
+    places: string;
   }>,
   res: Response,
   next: NextFunction
@@ -32,21 +33,33 @@ const signup = (
       new HttpError('Invalid inputs passed, please check your data.', 422)
     );
   }
-  const { name, email, password } = req.body;
-  if (USERS.find(u => u.email === email)) {
+  const { name, email, password, places } = req.body;
+  try {
+    if (await User.findOne({ email })) {
+      return next(
+        new HttpError('Could not create user, email already exists.', 422)
+      );
+    }
+
+    const createdUser = new User({
+      name,
+      email,
+      image: 'https://picsum.photos/200',
+      password,
+      places
+    });
+
+    try {
+      await createdUser.save();
+    } catch {
+      return next(new HttpError('Signing up failed, please try again.', 500));
+    }
+    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  } catch (error) {
     return next(
-      new HttpError('Could not create user, email already exists.', 422)
+      new HttpError('Signing up failed, please try again later.', 500)
     );
   }
-  const createdUser = {
-    id: nanoid(),
-    name,
-    email,
-    password
-  };
-
-  USERS.push(createdUser);
-  res.status(201).json({ user: createdUser });
 };
 
 const login = (
