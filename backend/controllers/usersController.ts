a0e36a1a34a1053be443bcbd63c1532614d512ee
Mubin Ansari <mubinansari910@gmail.com>
2022-12-17 16:26:00 +0530

    fix error status codes

--- backend/controllers/usersController.ts
+++ backend/controllers/usersController.ts
@@ -89,7 +89,7 @@ const login = async (
       return next(
         new HttpError(
           'Could not identify user, credentials seem to be wrong.',
-          401
+          403
         )
       );
     }
@@ -107,7 +107,7 @@ const login = async (
       return next(
         new HttpError(
           'Could not identify user, credentials seem to be wrong.',
-          401
+          403
         )
       );
     }
