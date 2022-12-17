40682423196ab4d9d627ee85a1a9fa888a6ea4c0
Mubin Ansari <mubinansari910@gmail.com>
2022-12-17 17:31:25 +0530

    add autologin and logout logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,20 +13,62 @@ import NewPlace from '@pages/NewPlace';
 import UpdatePlace from '@pages/UpdatePlace';
 import UserPlaces from '@pages/UserPlaces';
 import Users from '@pages/Users';
+import { useEffect } from 'react';
+
+let logoutTimer: NodeJS.Timeout;
 
 const App = () => {
   const [token, setToken] = useState<string | null>(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState<Date | null>();
   const [userId, setUserId] = useState<string | null>(null);
 
-  const login = useCallback((uid: string, token: string) => {
+  const login = useCallback((uid: string, token: string, expiration?: Date) => {
     setToken(token);
+    const expirationDate =
+      expiration || new Date(new Date().getTime() + 1000 * 60 * 60 * 6);
+    setTokenExpirationDate(expirationDate);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token,
+        expirationDate: expirationDate.toISOString()
+      })
+    );
     setUserId(uid);
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData') || '{}');
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expirationDate) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expirationDate)
+      );
+    }
+  }, [login]);
 
   let routes;
 
