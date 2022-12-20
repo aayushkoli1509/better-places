8f999072810a1474d8a3f6520f818e44dbf1ba01
Mubin Ansari <mubinansari910@gmail.com>
2022-12-20 18:48:38 +0530

    minor UI improvements to Auth page

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -115,6 +115,7 @@ const Auth = () => {
                 validators={[VALIDATOR_REQUIRE()]}
                 errorText='Please enter a name.'
                 onInput={inputHandler}
+                placeholder='Rick Rollins'
               />
               <ImageUpload
                 id='image'
@@ -133,6 +134,7 @@ const Auth = () => {
             errorText='Please enter a valid email.'
             validators={[VALIDATOR_EMAIL()]}
             onInput={inputHandler}
+            placeholder='rickroll@example.com'
           />
           <Input
             type='password'
@@ -142,6 +144,7 @@ const Auth = () => {
             errorText='Please enter a valid password (at least 6 characters).'
             validators={[VALIDATOR_MINLENGTH(6)]}
             onInput={inputHandler}
+            placeholder='********'
           />
           <Button disabled={!formState.isValid} type='submit'>
             {isLoginMode ? 'LOGIN' : 'SIGNUP'}
